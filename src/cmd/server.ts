efb34ef5c12b85faa1c1ead97fc82bce9bfe0e0c
Jerome Gravel-Niquet <jeromegn@gmail.com>
2018-01-31 08:30:48 -0500

    cli overhaul

--- src/cmd/server.ts
+++ src/cmd/server.ts
@@ -1,22 +1,25 @@
-import { parseConfig } from '../config'
 import * as path from 'path'
-import { Server } from '../server'
-import { FileStore } from '../app/stores/file'
 
-import { CommanderStatic } from 'commander';
+import { root } from './root'
 
-export function startServer(prg: CommanderStatic) {
-  const cwd = !prg.args[0]
-    ? process.cwd()
-    : prg.args[0].startsWith('/')
-      ? prg.args[0]
-      : path.resolve(process.cwd(), prg.args[0])
-
-  let conf = parseConfig(cwd)
-
-  if (prg.port) { conf.port = prg.port }
-
-  conf.appStore = new FileStore(cwd, { build: true })
-
-  new Server(conf).start()
+interface ServerOptions {
+  port?: string
 }
+
+root
+  .subCommand<ServerOptions, any>("server")
+  .description("Run the local Fly development server")
+  .option("-p, --port <port>", "Port to bind to")
+  .action((opts, args, rest) => {
+    const { parseConfig } = require('../config')
+    const { FileStore } = require('../app/stores/file')
+    const { Server } = require('../server')
+    const cwd = process.cwd()
+    let conf = parseConfig(cwd)
+
+    if (opts.port && opts.port.length) { conf.port = opts.port }
+
+    conf.appStore = new FileStore(cwd, { build: true })
+
+    new Server(conf).start()
+  })
