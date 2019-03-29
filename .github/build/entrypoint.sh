a411257cf4d64bc621620739828f4d930403cf3f
Michael Dwan <m@dwan.io>
2019-03-29 12:06:05 -0500

    x

--- .github/build/entrypoint.sh
+++ .github/build/entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 
 echo "HELLO"
 
-# echo "Install & Bootstrap"
-# yarn install --no-progress && yarn bootstrap
+echo "Install & Bootstrap"
+yarn install --no-progress && yarn bootstrap
 
-# echo "build"
-# yarn build && yarn bundle
+echo "build"
+yarn build && yarn bundle
